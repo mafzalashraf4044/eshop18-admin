3d1824b5c5cd4e1d6a9610ddc77ade56f757f9b7
Afzal Ashraf <afzalashraf.dev@gmail.com>
2018-09-14 10:59:02 +0500

    api_url constant added

--- src/constants.js
+++ src/constants.js
@@ -5,4 +5,4 @@ export const PAYMENTMETHODS_PAGE = 4;
 export const NEWS_PAGE = 5;
 export const REVIEWS_PAGE = 6;
 
-export const API_URL = "http://localhost:1337";
+export const API_URL = ARG_API_URL;
